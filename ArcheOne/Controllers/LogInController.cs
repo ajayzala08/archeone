e9f0b7aeb40061ec0394557bfb5dcf8bd79febef
nikunj-arche <nikunj.pandya@archesoftronix.com>
2023-05-25 15:39:09 +0530

    Created New Project ArcheOne - NP

---
+++ ArcheOne/Controllers/LogInController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ArcheOne.Controllers
+{
+	public class LogInController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
