404624b445a95cceccab8b2d4e22780899f93274
Preyansipatel24 <IMpatel@787>
2023-05-25 18:29:50 +0530

    PP - Helper/CommonResponse.cs, DbRepo add

--- ArcheOne/Controllers/LogInController.cs
+++ ArcheOne/Controllers/LogInController.cs
@@ -1,10 +1,16 @@
-﻿using ArcheOne.Models.Req;
+﻿using ArcheOne.Helper;
+using ArcheOne.Models.Req;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ArcheOne.Controllers
 {
 	public class LogInController : Controller
 	{
+		private readonly DbRepo _dbRepo;
+		public LogInController(DbRepo dbRepo)
+		{
+			_dbRepo = dbRepo;
+		}
 		public IActionResult Index()
 		{
 			return View();
@@ -12,7 +18,8 @@ namespace ArcheOne.Controllers
 		[HttpPost]
 		public IActionResult LogIn(LoginModel loginModel)
 		{
-
+			bool isUserExist = false;
+			isUserExist = _dbRepo.UserMstList().Where(x => x.UserName == loginModel.UserName && x.Password == loginModel.Password).ToList().Any();
 			return View("~/Dashboard/Index");
 		}
 	}
