0fe7e403236eab6212ae2366771d541e65d7df7d
Preyansipatel24 <IMpatel@787>
2023-05-29 14:05:19 +0530

    PP - commited on remember me

--- ArcheOne/Controllers/LogInController.cs
+++ ArcheOne/Controllers/LogInController.cs
@@ -1,13 +1,17 @@
 ﻿using ArcheOne.Helper;
 using ArcheOne.Models.Req;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 using System.Net;
+using System.Security.Claims;
 
 namespace ArcheOne.Controllers
 {
     public class LogInController : Controller
     {
         private readonly DbRepo _dbRepo;
+
         public LogInController(DbRepo dbRepo)
         {
             _dbRepo = dbRepo;
@@ -17,7 +21,7 @@ namespace ArcheOne.Controllers
             return View();
         }
         [HttpPost]
-        public IActionResult LogIn([FromBody] LoginModel loginModel)
+        public IActionResult LogIn1([FromBody] LoginModel loginModel)
         {
             CommonResponse commonResponse = new CommonResponse();
             var UserDetail = _dbRepo.UserMstList().Where(x => x.UserName == loginModel.UserName && x.Password == loginModel.Password).FirstOrDefault();
@@ -40,5 +44,45 @@ namespace ArcheOne.Controllers
         {
             return View();
         }
+
+
+
+
+        [HttpPost]
+        public IActionResult Login([FromBody] LoginModel model, string returnUrl = null)
+        {
+            if (ModelState.IsValid)
+            {
+                // Perform your authentication logic here
+
+                // If authentication is successful, create a ClaimsIdentity and sign in the user
+                var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, model.UserName),
+                // Add additional claims as needed
+            };
+
+                var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                var authProperties = new AuthenticationProperties
+                {
+                    IsPersistent = true
+                };
+
+                HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity), authProperties);
+
+                return RedirectToAction("Index", "Dashboard");
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Logout()
+        {
+            HttpContext.SignOutAsync(
+            CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Login", "Login");
+        }
     }
 }
+
--- ArcheOne/Program.cs
+++ ArcheOne/Program.cs
@@ -1,17 +1,35 @@
 using ArcheOne;
 using ArcheOne.Database.Entities;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
 builder.Services.AddControllersWithViews();
 string connString = builder.Configuration["ConnectionStrings:EntitiesConnection"];
 builder.Services.AddDbContext<ArcheOneDbContext>(options =>
 {
     options.UseSqlServer(connString);
 });
+
 builder.Services.DIScopes();
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+ .AddCookie(options =>
+ {
+     options.Cookie.Name = "RememberMecookie1"; // cookie name
+     options.Cookie.SameSite = SameSiteMode.None; // view where the cookie will be issued for the first time
+     options.ExpireTimeSpan = TimeSpan.FromDays(30); // time for the cookei to last in the browser
+     options.SlidingExpiration = true; // the cookie would be re-issued on any request half way through the ExpireTimeSpan
+     //options.EventsType = typeof(CookieAuthEvent);
+ });
+
+
+//builder.Services.AddAuthentication()
+//        .AddCookie();
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -19,6 +37,7 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
 }
+app.UseCookiePolicy();
 app.UseStaticFiles();
 
 app.UseRouting();
