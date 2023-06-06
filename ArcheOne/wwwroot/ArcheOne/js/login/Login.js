6095467079df8746478aeb7fe55163b20c003cd4
Preyansipatel24 <IMpatel@787>
2023-06-06 10:56:16 +0530

    PP- blockUI Changes

--- ArcheOne/wwwroot/ArcheOne/js/login/Login.js
+++ ArcheOne/wwwroot/ArcheOne/js/login/Login.js
@@ -1,36 +1,38 @@
 $(document).ready(function () {
- 
+
     //applyRequiredValidation();
     $("#btnLogin").click(function () {
-        
+        $.blockUI();
         var dataModel = {
             "UserName": $('#txtUserName').val(),
             "Password": $('#txtPassword').val(),
-           "RememberMe" : $('#rememberMeCheckbox').is(':checked')
+            "RememberMe": $('#rememberMeCheckbox').is(':checked')
         }
-        console.log(dataModel);
-        $.blockUI({message: "<h2>Please wait</p>"});
+        /*console.log(dataModel);*/
         if (validateRequiredFields()) {
             ajaxCall("Post", false, '/LogIn/LogIn', JSON.stringify(dataModel), function (result) {
-                console.log(result);
+                //console.log(result);
                 if (result.status == true) {
-                    Toast.fire({ icon: 'success', title: result.message });
                     RedirectToPage("/Dashboard/Index");
+                    Toast.fire({ icon: 'success', title: result.message });
                 }
                 else {
                     Toast.fire({ icon: 'error', title: result.message });
+                    $.unblockUI();
                 }
-                $.unblockUI();
             });
+        }
+        else {
+            $.unblockUI();
         }
     });
 
     $("#btnForgotPassword").click(function () {
+        $.blockUI();
         var dataModel = {
             "Email": $('#txtEmail').val()
         }
         console.log(dataModel);
-        $.blockUI({message: "<h2>Please wait</p>" });
         if (validateRequiredFields()) {
             ajaxCall("Post", false, '/LogIn/ForgotPassword', JSON.stringify(dataModel), function (result) {
                 if (result.status == true) {
@@ -40,19 +42,22 @@ $(document).ready(function () {
                 }
                 else {
                     Toast.fire({ icon: 'error', title: result.message });
+                    $.unblockUI();
                 }
-               $.unblockUI();
+
             });
+        }
+        else {
+            $.unblockUI();
         }
     });
 
     $("#btnResetPassword").click(function () {
+        $.blockUI();
         var dataModel = {
             "UserId": $('#txtUserId').val(),
             "NewPassword": $('#txtNewPassword').val()
         }
-        console.log(dataModel);
-        $.blockUI({ message: "<h2>Please wait</p>"});
         if (validateRequiredFields()) {
             ajaxCall("Post", false, '/LogIn/ResetPassword', JSON.stringify(dataModel), function (result) {
                 if (result.status == true) {
@@ -62,21 +67,22 @@ $(document).ready(function () {
                 }
                 else {
                     Toast.fire({ icon: 'error', title: result.message });
+                    $.unblockUI();
                 }
-                $.unblockUI();
             });
-           
+        }
+        else {
+            $.unblockUI();
         }
     });
 
     $("#btnChangePassword").click(function () {
+        $.blockUI();
         var dataModel = {
             "UserId": $('#txtUserId').val(),
             "OldPassword": $('#txtOldPassword').val(),
             "NewPassword": $('#txtNewPassword').val()
         }
-        console.log(dataModel);
-        $.blockUI({message: "<h2>Please wait</p>" });
         if (validateRequiredFields()) {
             ajaxCall("Post", false, '/LogIn/ChangePassword', JSON.stringify(dataModel), function (result) {
                 if (result.status == true) {
@@ -86,27 +92,29 @@ $(document).ready(function () {
                 }
                 else {
                     Popup_Toast.fire({ icon: 'error', title: result.message });
-                    //Popup_Toast.fire({ icon: 'error', title: result.message, showConfirmButton: true });
+                    $.unblockUI();
                 }
-                $.unblockUI();
             });
+        }
+        else {
+            $.unblockUI();
         }
     });
 
     $("#BtnLogout").click(function () {
-    ajaxCall("get", false, '/LogIn/Logout', '', function (result) {
-        if (result.status == true) {
-            console.log(result);
-            // Popup_Toast.fire({ icon: 'success', title: result.message });
-            RedirectToPage("/LogIn/LogIn");
-            //preventBack();
-            //noBack();
-        }
-        else {
-        }
-      
-    });
-    
+        ajaxCall("get", false, '/LogIn/Logout', '', function (result) {
+            if (result.status == true) {
+                console.log(result);
+                // Popup_Toast.fire({ icon: 'success', title: result.message });
+                RedirectToPage("/LogIn/LogIn");
+                //preventBack();
+                //noBack();
+            }
+            else {
+            }
+
+        });
+
 
     });
 });
@@ -122,10 +130,10 @@ function noBack() {
 }
 
 function getCookie(name) {
-   // var cookieName = encodeURIComponent(name) + '=';
+    // var cookieName = encodeURIComponent(name) + '=';
     var cookieName = encodeURIComponent(name);
     var cookieArray = document.cookie.split(';');
-    
+
     for (var i = 0; i < cookieArray.length; i++) {
         var cookie = cookieArray[i].trim(); // Trim any leading/trailing whitespace
 
