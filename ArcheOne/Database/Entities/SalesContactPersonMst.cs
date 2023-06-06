15e1b803e2b77b28e3f6f4654cf076b5d2f33a7b
Preyansipatel24 <IMpatel@787>
2023-06-06 12:14:20 +0530

    changes related to sales db - PP

---
+++ ArcheOne/Database/Entities/SalesContactPersonMst.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArcheOne.Database.Entities;
+
+public partial class SalesContactPersonMst
+{
+    public int Id { get; set; }
+
+    public int SalesLeadId { get; set; }
+
+    public string FirstName { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string? Designation { get; set; }
+
+    public string Mobile1 { get; set; } = null!;
+
+    public string? Mobile2 { get; set; }
+
+    public string Linkedinurl { get; set; } = null!;
+
+    public bool IsActive { get; set; }
+
+    public bool IsDelete { get; set; }
+
+    public int CreatedBy { get; set; }
+
+    public int UpdatedBy { get; set; }
+
+    public DateTime CreatedDate { get; set; }
+
+    public DateTime UpdatedDate { get; set; }
+}
---
+++ ArcheOne/Database/Entities/SalesLeadFollowUpMst.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArcheOne.Database.Entities;
+
+public partial class SalesLeadFollowUpMst
+{
+    public int Id { get; set; }
+
+    public int SalesLeadId { get; set; }
+
+    public int SalesContactPersonId { get; set; }
+
+    public DateTime FollowUpDateTime { get; set; }
+
+    public DateTime NextFollowUpDateTime { get; set; }
+
+    public int SalesLeadStatusId { get; set; }
+
+    public int SalesLeadActionId { get; set; }
+
+    public string? Notes { get; set; }
+
+    public bool IsActive { get; set; }
+
+    public bool IsDelete { get; set; }
+
+    public int CreatedBy { get; set; }
+
+    public int UpdatedBy { get; set; }
+
+    public DateTime CreatedDate { get; set; }
+
+    public DateTime UpdatedDate { get; set; }
+}
