68f6b6f6000c4170a41baad074d9366805bd044f
Ajay Zala <ajayzala08@gmail.com>
2023-06-26 09:31:20 +0530

    AZ : Salary Module Added ArcheOne/Controllers/SalaryController.cs ArcheOne/Views/Salary/Salary.cshtml ArcheOne/wwwroot/ArcheOne/js/Salary/Salary.js ArcheOne/ArcheOne.csproj ArcheOne/ArcheOne.csproj.user ArcheOne/wwwroot/ArcheOne/js/Profile/ChangeProfile.js

---
+++ ArcheOne/Controllers/SalaryController.cs
@@ -0,0 +1,108 @@
+﻿using ArcheOne.Helper.CommonHelpers;
+using ArcheOne.Helper.CommonModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Globalization;
+
+namespace ArcheOne.Controllers
+{
+    public class SalaryController : Controller
+    {
+        private readonly DbRepo _dbRepo;
+        private readonly CommonHelper _commonHelper;
+
+        public SalaryController(DbRepo dbRepo, CommonHelper commonHelper)
+        {
+            _dbRepo = dbRepo;
+            _commonHelper = commonHelper;
+        }
+
+        public IActionResult Salary()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Companies()
+        {
+            CommonResponse commonResponse = new CommonResponse();
+            try
+            {
+                var companyList = await _dbRepo.CompanyMstList().Select(x => new { x.Id, x.CompanyName }).ToListAsync();
+                if (companyList.Count > 0)
+                {
+                    commonResponse.Status = true;
+                    commonResponse.StatusCode = System.Net.HttpStatusCode.OK;
+                    commonResponse.Message = "Company list found";
+                    commonResponse.Data = companyList;
+                }
+                else
+                {
+                    commonResponse.Message = "Company list not found";
+                }
+            }
+            catch (Exception ex)
+            {
+                commonResponse.Message = ex.Message.ToString();
+            }
+
+            return Json(commonResponse);
+        }
+
+        [HttpGet]
+        public IActionResult Years()
+        {
+            CommonResponse commonResponse = new CommonResponse();
+            try
+            {
+                var yearList = Enumerable.Range(2010, (_commonHelper.GetCurrentDateTime().Year - 2010) + 1).Select((index, x) => new { Id = x + 1, SalaryYear = index }).ToList();
+                if (yearList.Count > 0)
+                {
+                    commonResponse.Status = true;
+                    commonResponse.StatusCode = System.Net.HttpStatusCode.OK;
+                    commonResponse.Message = "Year list found";
+                    commonResponse.Data = yearList;
+                }
+                else
+                {
+                    commonResponse.Message = "Year list not found";
+                }
+            }
+            catch (Exception ex)
+            {
+                commonResponse.Message = ex.Message.ToString();
+            }
+
+            return Json(commonResponse);
+        }
+
+
+        [HttpGet]
+        public IActionResult Months()
+        {
+            CommonResponse commonResponse = new CommonResponse();
+            try
+            {
+                var monthList = DateTimeFormatInfo.CurrentInfo.MonthNames.ToList().Select((x, index) => new { Id = index + 1, salaryMonth = x }).ToList();
+                if (monthList.Count > 0)
+                {
+                    commonResponse.Status = true;
+                    commonResponse.StatusCode = System.Net.HttpStatusCode.OK;
+                    commonResponse.Message = "Month list found";
+                    commonResponse.Data = monthList;
+                }
+                else
+                {
+                    commonResponse.Message = "Month list not found";
+                }
+            }
+            catch (Exception ex)
+            {
+                commonResponse.Message = ex.Message.ToString();
+            }
+
+            return Json(commonResponse);
+        }
+
+    }
+}
---
+++ ArcheOne/wwwroot/ArcheOne/js/Salary/Salary.js
@@ -0,0 +1,52 @@
+﻿$(document).ready(function () {
+    alert("JS loading....");
+    LoadCompanies();
+    LoadYears();
+    LoadMonths();
+    $.unblockUI();
+
+});
+
+function loadFile(event) {
+    $('#lblSalarySheet').html(event.target.files[0].name);
+}
+
+function LoadCompanies() {
+    ajaxCallWithoutDataType("GET", false, '/Salary/Companies', null, function (result) {
+        console.log(result);
+        if (result.status == true) {
+            debugger
+            $.each(result.data, function (data, value) {
+                $("#ddlCompany").append($("<option></option>").val(value.id).html(value.companyName));
+            })
+
+        }
+    });
+}
+
+
+function LoadYears() {
+    ajaxCallWithoutDataType("GET", false, '/Salary/Years', null, function (result) {
+        console.log(result);
+        if (result.status == true) {
+            debugger
+            $.each(result.data, function (data, value) {
+                $("#ddlyear").append($("<option></option>").val(value.id).html(value.salaryYear));
+            })
+
+        }
+    });
+}
+
+function LoadMonths() {
+    ajaxCallWithoutDataType("GET", false, '/Salary/Months', null, function (result) {
+        console.log(result);
+        if (result.status == true) {
+            debugger
+            $.each(result.data, function (data, value) {
+                $("#ddlmonth").append($("<option></option>").val(value.id).html(value.salaryMonth));
+            })
+
+        }
+    });
+}
