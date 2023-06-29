26386f33317cd34bf6ba1c66d94e2e16fa0be446
Preyansipatel24 <IMpatel@787>
2023-06-29 18:34:22 +0530

    PP - Leave/AddEditLeave changes

--- ArcheOne/Models/Req/AddUpdateLeaveReqModel.cs
+++ ArcheOne/Models/Req/AddUpdateLeaveReqModel.cs
@@ -10,9 +10,9 @@
 
         public DateTime EndDate { get; set; }
 
-        public TimeSpan StartTime { get; set; }
+        public string StartTime { get; set; }
 
-        public TimeSpan EndTime { get; set; }
+        public string EndTime { get; set; }
         public string Reason { get; set; } = null!;
 
 
