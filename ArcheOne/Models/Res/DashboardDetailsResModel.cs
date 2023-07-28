02a51d078bb114ede3ac87ffe1959d88463a2b90
Preyansipatel24 <IMpatel@787>
2023-07-28 18:34:08 +0530

    PP - commited Dashboard/Index.cshtml changes

---
+++ ArcheOne/Models/Res/DashboardDetailsResModel.cs
@@ -0,0 +1,26 @@
+﻿namespace ArcheOne.Models.Res
+{
+    public class DashboardDetailsResModel
+    {
+        public int InterviewRoundCount { get; set; }
+        public int UserCount { get; set; }
+        public int SalesLeadsCount { get; set; }
+        public int ProjectCount { get; set; }
+        public int ProjectCompletedCount { get; set; }
+        public int ProjectToDoCount { get; set; }
+        public int ProjectInProgressCount { get; set; }
+        public int SalesOpportunityCount { get; set; }
+        public int ClosureCount { get; set; }
+        public int SubmissionCount { get; set; }
+        public int BDCount { get; set; }
+        public int TeamCount { get; set; }
+        public int PendingLeaveCount { get; set; }
+        public int PendingResumeApprovalCount { get; set; }
+        public int InHouseRequirementCount { get; set; }
+        public int ClientRequirementCount { get; set; }
+        public int ActiveRequirementCount { get; set; }
+        public int InActiveRequirementCount { get; set; }
+        public int OnHoldRequirementCount { get; set; }
+        public int CloseRequirementCount { get; set; }
+    }
+}
